84f0695916973d421aba33807e84b7b7e6b95e66
Vitalijus S <siaurys@gmail.com>
2023-12-08 16:20:00 +0200

    Complete Excercise Division

---
+++ Java/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,12 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+        division(18, 6);
+    }
+
+    public static void division(int numerator, int denominator) {
+        System.out.println((double) numerator / denominator);
+    }
+}
