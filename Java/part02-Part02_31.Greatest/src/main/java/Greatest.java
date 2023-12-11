584388c3d5930cf2cf3c1cc7dceadd26e8333d9d
Vitalijus S <siaurys@gmail.com>
2023-12-11 10:45:35 +0200

    Update exercise Greatest

--- Java/part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ Java/part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -2,15 +2,18 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        if (number1 >= number2) {
-            if (number1 > number3) {
-                return number1;
-            }
-        } else if (number2 > number3) {
-            return number2;
+        int greatest = number1;
+
+        if (number2 > greatest) {
+            greatest = number2;
         }
-        return number3;
-    }
+
+        if (number3 > greatest) {
+            greatest = number3;
+        }
+
+        return greatest;
+}
 
     public static void main(String[] args) {
         int result = greatest(2, 7, 3);
