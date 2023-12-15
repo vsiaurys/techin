0c9fd1d87620d150fbff7f778ae78f749deb4500
Vitalijus S <siaurys@gmail.com>
2023-12-15 09:53:48 +0200

    Complete exercise Your first account

---
+++ Java/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,10 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        Account artosAccount = new Account("Arto's account", 100.00);
+
+        artosAccount.deposit(20);
+        System.out.println(artosAccount);
+    }
+}
