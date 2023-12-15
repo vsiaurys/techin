9aa06b601ef3219cc44791646250546045ee193a
Vitalijus S <siaurys@gmail.com>
2023-12-15 15:31:51 +0200

    Complete exercise Agent

---
+++ Java/part04-Part04_13.Agent/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Agent bond = new Agent("James", "Bond");
+        System.out.println(bond);
+
+        Agent ionic = new Agent("Ionic", "Bond");
+        System.out.println(ionic);
+    }
+}
