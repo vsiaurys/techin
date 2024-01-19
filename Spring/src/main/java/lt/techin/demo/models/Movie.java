b91f17437459d0dcbbe038d15e12bad4194f86f0
Vitalijus S <siaurys@gmail.com>
2024-01-19 15:24:22 +0200

    Create Movie model

---
+++ Spring/src/main/java/lt/techin/demo/models/Movie.java
@@ -0,0 +1,17 @@
+package lt.techin.demo.models;
+
+public class Movie {
+    private long id;
+    private String title;
+    private String director;
+    private short yearReleased;
+    private short lengthMinutes;
+
+    public Movie(long id, String title, String director, short yearReleased, short lengthMinutes) {
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.yearReleased = yearReleased;
+        this.lengthMinutes = lengthMinutes;
+    }
+}
