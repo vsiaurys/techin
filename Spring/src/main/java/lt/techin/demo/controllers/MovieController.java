6623c7eccf4059db7b6ed79bf68a96105728ff76
Vitalijus S <siaurys@gmail.com>
2024-01-19 14:00:26 +0200

    Update getMovies() method with ArrayList

--- Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MovieController {
+    private ArrayList<String> movies = new ArrayList<>(
+            List.of("Toy Story", "A bug's life", "Toy Story 2", "Monsters, Inc.", "Finding Nemo"));
 
     @GetMapping("/movies")
-    public String getMovie() {
-        return "Toy Story";
+    public ArrayList<String> getMovies() {
+        return this.movies;
     }
 }
