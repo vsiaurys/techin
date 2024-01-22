9c2b5066f0c92b115b90f3aa5786ded4dc851395
Vitalijus S <siaurys@gmail.com>
2024-01-22 15:34:54 +0200

    Complete exercise Interface In A Box (Parts 3,4)

--- Java/part09-Part09_06.InterfaceInABox/src/main/java/Box.java
+++ Java/part09-Part09_06.InterfaceInABox/src/main/java/Box.java
@@ -1,22 +1,37 @@
-public class Box {
-    double capacity;
-    double weight;
-    int items;
+import java.util.ArrayList;
+
+public class Box implements Packable {
+    private double capacity;
+    private ArrayList<Packable> items;
 
     public Box(double capacity) {
         this.capacity = capacity;
-        weight = 0.0;
-        items = 0;
+        items = new ArrayList<Packable>();
     }
 
-    public void add(Packable item) {
-        if (capacity >= weight + item.weight()) {
-            weight += item.weight();
-            items++;
+    public void add(Packable packable) {
+        if (capacity >= weight() + packable.weight()) {
+            items.add(packable);
         }
     }
 
+    public double weight() {
+        double weight = 0;
+
+        for (Packable item : items) {
+            weight += item.weight();
+        }
+
+        return weight;
+    }
+
     public String toString() {
-        return "Box: " + items + " items, total weight " + weight + " kg";
+        int itemsCount = 0;
+
+        for (Packable item : items) {
+            itemsCount++;
+        }
+
+        return "Box: " + itemsCount + " items, total weight " + weight() + " kg";
     }
 }
--- Java/part09-Part09_06.InterfaceInABox/src/main/java/Main.java
+++ Java/part09-Part09_06.InterfaceInABox/src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
         box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
         box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
 
+        
         System.out.println(box);
     }
 }
