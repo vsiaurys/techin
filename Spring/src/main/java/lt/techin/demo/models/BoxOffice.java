93eae16fe9c3a7a47187a446f123a2ea14994485
Vitalijus <siaurys@gmail.com>
2024-01-25 10:13:32 +0200

    Add getters and setters to Boxofiice class

--- Spring/src/main/java/lt/techin/demo/models/BoxOffice.java
+++ Spring/src/main/java/lt/techin/demo/models/BoxOffice.java
@@ -22,4 +22,36 @@ public class BoxOffice {
 
     public BoxOffice() {
     }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public long getDomesticSales() {
+        return domesticSales;
+    }
+
+    public long getInternationalSales() {
+        return internationalSales;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public void setDomesticSales(long domesticSales) {
+        this.domesticSales = domesticSales;
+    }
+
+    public void setInternationalSales(long internationalSales) {
+        this.internationalSales = internationalSales;
+    }
 }
