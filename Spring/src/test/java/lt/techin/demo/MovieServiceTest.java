0412eebaefa73183ec4d16a60948105c58a3bb13
Vitalijus S <siaurys@gmail.com>
2024-01-26 10:03:56 +0200

    Replace isEqualTo with containsExactly

--- Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
+++ Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
@@ -32,14 +32,6 @@ class MovieServiceTest {
                 "Stephen Spielberg", (short) 1999, (short) 120));
         List<Movie> movies = this.movieService.findAllMovies();
 
-        then(movies.get(0).getTitle()).isEqualTo(savedMovie1.getTitle());
-        then(movies.get(0).getDirector()).isEqualTo(savedMovie1.getDirector());
-        then(movies.get(0).getYearReleased()).isEqualTo(savedMovie1.getYearReleased());
-        then(movies.get(0).getLengthMinutes()).isEqualTo(savedMovie1.getLengthMinutes());
-
-        then(movies.get(1).getTitle()).isEqualTo(savedMovie2.getTitle());
-        then(movies.get(1).getDirector()).isEqualTo(savedMovie2.getDirector());
-        then(movies.get(1).getYearReleased()).isEqualTo(savedMovie2.getYearReleased());
-        then(movies.get(1).getLengthMinutes()).isEqualTo(savedMovie2.getLengthMinutes());
+        then(movies).containsExactly(savedMovie1, savedMovie2);
     }
 }
