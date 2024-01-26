62f57265bfea9b91799f0d9165d136148d7ced6c
Vitalijus S <siaurys@gmail.com>
2024-01-26 09:29:46 +0200

    Update MovieServiceTest

--- Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
+++ Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
@@ -16,18 +16,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
-public class MovieServiceTest {
+class MovieServiceTest {
     @Autowired
     private MovieService movieService;
     @Autowired
     private MovieRepository movieRepository;
+
     @Test
-    void findAllMovies_saveMovies_returned(){
-        Movie savedMovie1=this.movieRepository
-                .save(new Movie("Madagascar", "Stephen Spielberg", (short)2005, (short)60));
-        Movie savedMovie2=this.movieRepository
-                .save(new Movie("HOme Alone", "Stephen Spielberg", (short)1999, (short)120));
+    void findAllMovies_saveMovies_returned() {
+        Movie savedMovie1 = this.movieRepository
+                .save(new Movie("Madagascar", "Stephen Spielberg", (short) 2005, (short) 60));
+        Movie savedMovie2 = this.movieRepository
+                .save(new Movie("HOme Alone", "Stephen Spielberg", (short) 1999, (short) 120));
         List<Movie> movies = this.movieService.findAllMovies();
+
         assertThat(movies.get(0).getTitle()).isEqualTo(savedMovie1.getTitle());
+        assertThat(movies.get(0).getDirector()).isEqualTo(savedMovie1.getDirector());
+        assertThat(movies.get(0).getYearReleased()).isEqualTo(savedMovie1.getYearReleased());
+        assertThat(movies.get(0).getLengthMinutes()).isEqualTo(savedMovie1.getLengthMinutes());
+
+        assertThat(movies.get(1).getTitle()).isEqualTo(savedMovie2.getTitle());
+        assertThat(movies.get(1).getDirector()).isEqualTo(savedMovie2.getDirector());
+        assertThat(movies.get(1).getYearReleased()).isEqualTo(savedMovie2.getYearReleased());
+        assertThat(movies.get(1).getLengthMinutes()).isEqualTo(savedMovie2.getLengthMinutes());
+
     }
 }
