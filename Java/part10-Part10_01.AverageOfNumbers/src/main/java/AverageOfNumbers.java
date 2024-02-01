d77c0f85edbe91be08c6188a6f6ccafa4c83cd26
Vitalijus S <siaurys@gmail.com>
2024-02-01 10:10:56 +0200

    Complete exercise Average of Numbers

---
+++ Java/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> inputs = new ArrayList<>();
+
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
+
+            inputs.add(row);
+        }
+        double averageOfNumbers = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+
+        System.out.println("average of the numbers:" + averageOfNumbers);
+    }
+}
+
