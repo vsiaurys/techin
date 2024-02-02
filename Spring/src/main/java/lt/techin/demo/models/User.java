91eb2c7fe23be8ad2c5426c835a25a15e4fa1280
Vitalijus S <siaurys@gmail.com>
2024-02-02 15:50:58 +0200

    Modify UserRepository

--- Spring/src/main/java/lt/techin/demo/models/User.java
+++ Spring/src/main/java/lt/techin/demo/models/User.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Entity
 @Table(name = "Users")
 public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY);
     private long id;
