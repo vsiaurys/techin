d2c0f5269a5b869a5030dea80885de5c3f17fc99
Vitalijus S <siaurys@gmail.com>
2024-02-05 12:02:31 +0200

    Update User model

--- Spring/src/main/java/lt/techin/demo/controllers/UserController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/UserController.java
@@ -2,11 +2,13 @@ package lt.techin.demo.controllers;
 
 import lt.techin.demo.models.User;
 import lt.techin.demo.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -26,10 +28,14 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = this.userService.saveUser(user);
 
-        return this.userService.saveUser(user);
+        return ResponseEntity.created(ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri()).body(savedUser);
     }
 }
