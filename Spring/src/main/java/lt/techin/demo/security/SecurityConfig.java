211156daf451088d7c4b3f909207888a74e06de0
Vitalijus S <siaurys@gmail.com>
2024-02-05 10:40:45 +0200

    Create bean SecurityFilterChain in SecurityConfig

--- Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
+++ Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
@@ -2,13 +2,16 @@ package lt.techin.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
+    //    @Bean
 //    public UserDetailsService userDetailsService() {
 //        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
 //
@@ -21,6 +24,16 @@ public class SecurityConfig {
 //
 //        return userDetailsManager;
 //    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated()).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
