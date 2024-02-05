92b03e3892fec03ddcb865807063dba09e386830
Vitalijus S <siaurys@gmail.com>
2024-02-05 15:10:50 +0200

    Update securityFilterChain in SecurityConfig class

--- Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
+++ Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
@@ -33,6 +33,16 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.GET, "/users")
                         .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/movies").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/movies/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/movies")
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/movies")
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/movies/{id}")
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/movies/{id}")
+                        .hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
