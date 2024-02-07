fcaebdacf3ec417a6aba7ea281dd0651318a8686
Vitalijus S <siaurys@gmail.com>
2024-02-07 13:40:36 +0200

    Create deleteDirectorMovie in DirectorMovieController

--- Spring/src/main/java/lt/techin/demo/controllers/DirectorMovieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/DirectorMovieController.java
@@ -7,6 +7,7 @@ import lt.techin.demo.models.Movie;
 import lt.techin.demo.services.DirectorMovieService;
 import lt.techin.demo.services.DirectorService;
 import lt.techin.demo.services.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,5 +41,35 @@ public class DirectorMovieController {
     @PostMapping("/directorsmovies")
     public DirectorMovie insertDirectorMovie(@RequestBody DirectorMovie directorMovie) {
         return this.directorMovieService.saveDirectorMovie(directorMovie);
+    }
+
+    /*
+
+    @PutMapping("/movies/{id}")
+    public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie, @PathVariable long id) {
+        if (this.movieService.existsMovieById(id)) {
+            Movie movieFromDb = this.movieService.findMovieById(id);
+
+            movieFromDb.setTitle(movie.getTitle());
+            movieFromDb.setDateReleased(movie.getDateReleased());
+            movieFromDb.setLengthMinutes(movie.getLengthMinutes());
+
+            return ResponseEntity.ok(this.movieService.saveMovie(movieFromDb));
+        }
+        Movie savedMovie = this.movieService.saveMovie(movie);
+
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedMovie.getId()).toUri()).body(savedMovie);
+    }
+*/
+    @DeleteMapping("/directors/{directorId}/movies/{movieId}")
+    public ResponseEntity<Void> deleteDirectorMovie(@PathVariable DirectorMovieId directorMovieId) {
+
+        if (this.directorMovieService.existsDirectorMovieById(directorMovieId)) {
+            this.directorMovieService.deleteDirectorMovieById(directorMovieId);
+
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
